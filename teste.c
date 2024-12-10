56673c9489b470931093ea11a077cea78d379726
yV1ctorr <yagomelo@yahoo.com>
2024-12-10 11:42:48 -0300

    vetor-struct

--- teste.c
+++ teste.c
@@ -1,37 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+
 // declaração de struct
 struct pessoa{
-    char nome[20];
+    char nome[30];
     int idade;
     float altura;
 };
+
 // redefine a struct("pessoa") para "Pessoa" 
 typedef struct pessoa Pessoa;
 
-int  main() {    
-    // criando variavel dentro da struct
-    Pessoa pessoa1;
-    // atribuindo valores aos campos
-    printf("Inicio\n");
-    printf("Nome:%s\n", pessoa1.nome);
-    printf("Idade: %d\n", pessoa1.idade);
-    printf("Altura: %.2f/n", pessoa1.altura);
+int  main() {
 
-    // usuario atribuindo valores
-    printf("Digite seu nome:\n");
-    scanf("%19[^\n]s", pessoa1.nome);
-    fflush(stdin);
-    printf("DIgite a sua idade:\n");
-    scanf("%d", &pessoa1.idade);
-    printf("Digite a sua altura:\n");
-    scanf("%f", &pessoa1.altura);
+    // criando um vetor dentro da struct
+    int i;
+    int tam = 3;
+    Pessoa lista[3];
 
-    printf("PESSOA 1 \n");
-    printf("Nome: %s\n", pessoa1.nome);
-    printf("Idade: %i\n", pessoa1.idade);
-    printf("Altura:%.2f\n", pessoa1.altura);
+    // atribuindo valores no vetor
+    for (i = 0; i < 3; i++){
+        printf("Insira dados (%d):\n", i+1);
+        printf("Nome: \n");
+        scanf("%30[^\n]s", &lista[i].nome);
+        fflush(stdin);
 
+        puts("Idade: ");
+        scanf("%d", &lista[i].idade);
+        fflush(stdin);
+
+        puts("Altura: ");
+        scanf("%f", &lista[i].altura);
+        fflush(stdin);
+    }
+    for (i = 0; i < tam; i++){
+        for (tam = 0; tam < 3; tam++){
+            printf("Nome: %s\n", lista[i].nome);
+            printf("Idade: %d\n", lista[i].idade);
+            printf("Altura: %f\n", lista[i].altura);
+        }
+    }
+    
 
     return 0;
 }
