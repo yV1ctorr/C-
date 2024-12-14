d8514b2091b01f586f9c997f976c583fdca2ce5f
yago victor <142534616+yV1ctorr@users.noreply.github.com>
2024-12-14 14:06:04 -0300

    Atualizar o

---
+++ uf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include <string.h>
+
+int main() {
+
+  char senha[10];
+  senha = "senha";
+  char senhaInserida;
+  
+  printf("Digite sua senha: \n");
+  scanf("[^\n]s", senhaInserida);
+  strcmp()
+  return 0;
+}
